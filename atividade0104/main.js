f54ae36bed9f4655f87b6b97e870f1729f45a201
Vini <ovini.15515@gmail.com>
2025-04-07 22:29:51 -0300

    atividade finalizada

--- atividade0104/main.js
+++ atividade0104/main.js
@@ -14,12 +14,17 @@ campoText.addEventListener('keyup', function (event) {
 
 })
 
-btn.onclick = function(){
+var minhaTabela = document.getElementById("tabela").getElementsByTagName('tbody')[0]
+
+btn.onclick = function () {
 
     let texto = document.getElementById("campoTxt").value
 
-    document.getElementById("tabela").innerHTML += `<tr> ${texto}</tr>`
-    document.getElementById("opcoesLista").innerHTML += `<option value="${texto}">${texto}</option>`
+    let novaCol = minhaTabela.insertRow();
+    let row = novaCol.insertCell(0);
+    row.textContent = `${texto}`;
+
+    document.getElementById("opcoesLista").innerHTML += `<option value="${texto}" ">${texto}</option>`
 
     contador++
 }
@@ -27,11 +32,64 @@ btn.onclick = function(){
 
 var marcar = document.getElementById("btnMarcar")
 
-marcar.onclick = function() {
-    let elemento = document.getElementById("opcoesLista").value
-    for (let i = 0; i < contador; i++) {
-        if(elemento == document.getElementById("tabela").textContent[i]){
-            alert("elemento encontrado")
+    marcar.onclick = function () {
+        let termoPraProcurar = document.getElementById('opcoesLista').value.toLowerCase();
+        let tabela = document.getElementById('tabela');
+        let tbody = tabela.getElementsByTagName('tbody')[0];
+        let linhas = tbody.getElementsByTagName('tr');
+        
+        for (let row of linhas) {
+          let item = row.getElementsByTagName('td');
+          let valorItem = item[0]; 
+          
+         
+          if (valorItem && valorItem.textContent.toLowerCase().includes(termoPraProcurar)) {
+            row.style.backgroundColor = 'yellow'; 
+          } else {
+            row.style.backgroundColor = ''; 
+          }
         }
+    }
+
+var desMarcar = document.getElementById("btnDesMarcar")
+
+desMarcar.onclick = function () {
+    let termoPraProcurar = document.getElementById('opcoesLista').value.toLowerCase();
+    let tabela = document.getElementById('tabela');
+    let tbody = tabela.getElementsByTagName('tbody')[0];
+    let linhas = tbody.getElementsByTagName('tr');
+    
+    for (let row of linhas) {
+      let item = row.getElementsByTagName('td');
+      let valorItem = item[0]; 
+      
+     
+      if (valorItem && valorItem.textContent.toLowerCase().includes(termoPraProcurar)) {
+        row.style.backgroundColor = 'white'; 
+      } else {
+        row.style.backgroundColor = ''; 
+      }
+    }
+}
+
+
+var excluir = document.getElementById("btnExcluir")
+
+excluir.onclick = function () {
+    let termoPraProcurar = document.getElementById('opcoesLista').value.toLowerCase();
+    let tabela = document.getElementById('tabela');
+    let tbody = tabela.getElementsByTagName('tbody')[0];
+    let linhas = tbody.getElementsByTagName('tr');
+    
+    for (let row of linhas) {
+      let item = row.getElementsByTagName('td');
+      let valorItem = item[0]; 
+      
+     
+      if (valorItem && valorItem.textContent.toLowerCase().includes(termoPraProcurar)) {
+        row.innerHTML = ``
+      } else {
+        row.style.backgroundColor = ''; 
+      }
     }
 }
